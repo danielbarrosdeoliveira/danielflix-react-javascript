e4a329f376624f23ecad13f23a92c27b22ad8d2d
Daniel Oliveira <danielbarrosdeoliveira@outlook.com>
2020-07-28 14:16:22 -0300

    alteração da descrição banner

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Menu />
       <BannerMain
         url="https://youtu.be/ObQMysW58NA"
-        videoDescription="É uma música no estilo de RAP, criado pelo pessoal do 7 minutoz para o público Otaku, conta um pouco sobre Naruto, protagonista do anime Naruto, que cresceu sem pai e sem mãe, sob olhares de pessoas que o julgavam por ter um demônio selado, que atacou aldeia onde morava, mas isso não o impediu se seguir seus sonhos."
+        videoDescription="É uma música no estilo de RAP, criado pelo pessoal do 7 minutoz para o público Otaku, conta um pouco sobre Naruto, protagonista do anime que cresceu sem pai e mãe, sob olhares de pessoas que o julgavam por ter um demônio selado, que atacou aldeia onde morava, mas isso não o impediu de seguir seus sonhos."
         videoTitle="Naruto - O Sétimo Hokage"
       />
       {bancoDeDados.categorias.map((categoria, index) => (
