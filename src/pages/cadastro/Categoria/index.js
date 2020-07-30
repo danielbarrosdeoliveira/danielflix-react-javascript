a9689559a734cd2aa986ff2ea44eb7d5645a17e7
Daniel Oliveira <danielbarrosdeoliveira@outlook.com>
2020-07-30 13:59:12 -0300

    adição de formulário a roda de casdtro de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,12 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
-const CadastroCategoria = () => {
+function CadastroCategoria() {
+  const initialValues = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorys, setCategorys] = useState([]);
+  const [values, setValues] = useState(initialValues);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form
+        onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategorys([...categorys, values]);
+
+          setValues(initialValues);
+        }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <button>Cadastrar</button>
+      </form>
+
+      <ul>
+        {categorys.map((category, indice) => {
+          return <li key={`${category}${indice}`}>{category.nome}</li>;
+        })}
+      </ul>
+
+      <Link to="/">Ir para home</Link>
     </PageDefault>
   );
-};
+}
 
 export default CadastroCategoria;
